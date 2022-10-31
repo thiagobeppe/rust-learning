5cbd9be9cfce43d0a3d51b2ccd95293c92b16c0e
tbeppe <thiagoabeppe@gmail.com>
2022-10-31 08:37:23 -0300

    adding our first cargo projecty

---
+++ CodeShow/Aula-1/cargo_new/project/target/debug/deps/project-2263dfba1b699cad.d
@@ -0,0 +1,5 @@
+/Users/thiagobeppe/Documents/studies/Rust/rust-learning/CodeShow/Aula-1/cargo_new/project/target/debug/deps/project-2263dfba1b699cad: src/main.rs
+
+/Users/thiagobeppe/Documents/studies/Rust/rust-learning/CodeShow/Aula-1/cargo_new/project/target/debug/deps/project-2263dfba1b699cad.d: src/main.rs
+
+src/main.rs:
